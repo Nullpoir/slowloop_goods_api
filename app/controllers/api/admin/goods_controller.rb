5972c40051553210749832502d128586620c8ca4
nullpoir <nullpoir0805@gmail.com>
2022-10-10 00:11:14 +0900

    管理側index

--- app/controllers/api/admin/goods_controller.rb
+++ app/controllers/api/admin/goods_controller.rb
@@ -1,4 +1,11 @@
 class Api::Admin::GoodsController < AdminController
   def index
+    render json: goods, each_serializer: GoodSerializer
+  end
+
+  private
+
+  def goods
+    @goods ||= Good.order(id: :asc)
   end
 end

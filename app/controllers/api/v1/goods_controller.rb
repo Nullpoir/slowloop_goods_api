6ea96d4c9c1237c0b9a491c2a58a3384371d7abb
nullpoir <nullpoir0805@gmail.com>
2022-10-10 00:11:08 +0900

    公開側index

--- app/controllers/api/v1/goods_controller.rb
+++ app/controllers/api/v1/goods_controller.rb
@@ -1,4 +1,11 @@
 class Api::V1::GoodsController < ApplicationController
   def index
+    render json: goods, each_serializer: GoodSerializer
+  end
+
+  private
+
+  def goods
+    @goods ||= Good.order(id: :asc)
   end
 end

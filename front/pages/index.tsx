adee5b4e9f996d70b649f731ef52672c4a291b74
nullpoir <nullpoir0805@gmail.com>
2022-10-31 00:56:16 +0900

    中間点

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,9 +1,39 @@
 import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { AppBar, Card, Container, CssBaseline, Button  } from '@mui/material';
+
+const theme = createTheme();
 
 const Home = (): JSX.Element => {
   return(
     <div>
-      aa
+      <AppBar position="static">
+        
+        SlowLoop
+      </AppBar>
+
+      <Container>
+        <Card>
+          グッズ
+          <Button>
+            詳細
+          </Button>
+        </Card>
+
+        <Card>
+          グッズ
+          <Button>
+            詳細
+          </Button>
+        </Card>
+
+        <Card>
+          グッズ
+          <Button>
+            詳細
+          </Button>
+        </Card>
+      </Container>
     </div>
   )
 }

a443ff830b7f9c7d9a4cf7166063b9cedad3594d
nullpoir <nullpoir0805@gmail.com>
2022-11-03 02:46:42 +0900

    いい感じに

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,38 +1,55 @@
 import * as React from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { AppBar, Card, Container, CssBaseline, Button  } from '@mui/material';
+import { styled } from '@mui/material/styles'
+import { AppBar, Card, Container, Box, CssBaseline, Button  } from '@mui/material';
 
+// styling
+const StyledCard = styled(Card)(({ theme }) => ({
+  margin: theme.spacing(1),
+  boxShadow: theme.shadows[3],
+  color: theme.palette.error.main,
+  minHeight: "250px",
+  flexBasis: "250px"
+  
+}))
+
+const Header = styled(AppBar)(({ theme }) => ({
+  height: "50px",
+  position: "sticky"
+}))
+
+// JSX
 const Home = (): JSX.Element => {
   return(
-    <div>
-      <AppBar position="static">
-        
-        SlowLoop Goods
-      </AppBar>
+    <>
+      <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: "100vh" }}>
+        <Header>
+          SlowLoop Goods
+        </Header>
+        <CssBaseline/>
+        <Box sx={{ display: 'flex', flexWrap: "wrap", minWidth: "100vw"}}>
+          <StyledCard>
+            グッズ名
+            <Button>
+              詳細
+            </Button>
+          </StyledCard>
 
-      <Container>
-        <Card>
-          グッズ
-          <Button>
-            詳細
-          </Button>
-        </Card>
+          <StyledCard>
+            グッズ
+            <Button>
+              詳細
+            </Button>
+          </StyledCard>
 
-        <Card>
-          グッズ
-          <Button>
-            詳細
-          </Button>
-        </Card>
-
-        <Card>
-          グッズ
-          <Button>
-            詳細
-          </Button>
-        </Card>
-      </Container>
-    </div>
+          <StyledCard>
+            グッズ
+            <Button>
+              詳細
+            </Button>
+          </StyledCard>
+        </Box>
+      </Box>
+    </>
   )
 }
 

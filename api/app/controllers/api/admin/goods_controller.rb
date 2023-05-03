1911f51f2464c7b5d71fce408df852775a44d723
nullpoir <nullpoir0805@gmail.com>
2023-05-03 16:48:21 +0900

    RSpec + ransack usage

--- api/app/controllers/api/admin/goods_controller.rb
+++ api/app/controllers/api/admin/goods_controller.rb
@@ -1,61 +1,60 @@
-class Api::Admin::GoodsController < AdminController
-  before_action :good, only: %i(show update destroy)
-  before_action :init_good, only: %i(create)
+module Api
+  class GoodsController < ApplicationController
+    before_action :good, only: %i(show update destroy)
+    before_action :init_good, only: %i(create)
 
-  def index
-    render json: goods, each_serializer: GoodSerializer
-  end
+    def index
+      render json: resources, each_serializer: GoodSerializer
+    end
 
-  def show
-    render_json
-  end
+    def show
+      render_json
+    end
 
-  def create
-    @good.save!
-    render_json
-  end
+    def create
+      @good.save!
+      render_json
+    end
 
-  def update
-    @good.update!(good_params)
-    render_json
-  end
-  
-  def destroy
-    @good.destroy!
-    render_success_destroy
-  end
+    def update
+      @good.update!(good_params)
+      render_json
+    end
+    
+    def destroy
+      @good.destroy!
+      render_success_destroy
+    end
 
-  private
+    private
 
-  def render_json
-    render json: @good, serializer: GoodSerializer
-  end
+    def render_json
+      render json: @good, serializer: GoodSerializer
+    end
 
-  def goods
-    @goods ||= Good.ransack(params[:q])
-                   .result 
-                   .order(order_by => direction)
-                   .limit(limit).offset(offset)
-  end
+    def resources_scope
+      Good.all
+    end
 
-  def init_good
-    @good = Good.new(good_params)
-  end
+    def init_good
+      @good = Good.new(good_params)
+    end
 
-  def good
-    @good = Good.find(params[:id])
-  end
+    def good
+      @good = Good.find(params[:id])
+    end
 
-  def good_params
-    params.require(:good)
-          .permit(
-            :name,
-            :description,
-            :isbn,
-            :jan,
-            :shopping_url,
-            :released_at,
-            :production_ended_at
-          )
+    def good_params
+      params.require(:good)
+            .permit(
+              :name,
+              :description,
+              :isbn,
+              :jan,
+              :shopping_url,
+              :released_at,
+              :production_ended_at
+            )
+    end
   end
 end
